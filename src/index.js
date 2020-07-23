bb6e50c36f312d68cdec814850f64eebec08878e
Paola <spsanchez0115@gmail.com>
2020-07-23 18:50:53 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import createPatient from "./controller/patient/createPatient";
 connect();
 
 async function main() {
-  try {
-    const patient = {
-      id_global_patient: "G1",
-      id_local_patient: "id_local_patient",
-    };
-    const savedPatient = await createPatient(patient);
-    return savedPatient;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const patient = {
+            id_global_patient: "G1",
+            id_local_patient: "id_local_patient",
+        };
+        const savedPatient = await createPatient(patient);
+        return savedPatient;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 main();
