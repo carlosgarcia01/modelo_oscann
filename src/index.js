6233ff773c4b255151481869f5ee5778cd92d667
carlosgarcia01 <cg94947@gmail.com>
2020-07-23 18:46:30 -0500

    firts

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import { connect } from "./database";
 import createPatient from "./controller/patient/createPatient";
+import { getPatients } from "./controller/patient/getPatients";
 connect();
 
 async function main() {
   try {
     const patient = {
-      id_global_patient: "G1",
-      id_local_patient: "id_local_patient",
+      id_global_patient: "G3",
+      id_local_patient: "id_local_patient 3",
     };
     const savedPatient = await createPatient(patient);
+    const result = await getPatients();
+    console.log(result);
     return savedPatient;
   } catch (err) {
     console.log(err);
   }
 }
+
 
 main();
