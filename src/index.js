43f2455b0df2a4e5a62ae19efad94be58722faa4
GaboDotCodes <gabedu2000@gmail.com>
2020-07-23 17:56:28 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
-import { connect }  from "./database";
-import createPatient from './controller/patient/createPatient';
+import { connect } from "./database";
+import createPatient from "./controller/patient/createPatient";
 connect();
 
 async function main() {
-  try{
-      const patient = {
-      _id: "1",
+  try {
+    const patient = {
       id_global_patient: "G1",
-      ids_patient_local:[],
-      update_date: Date.now(),
-      id_estudy: ["Estudio1","Estudio2"],
-      id_PatientDemographic: "datos Paciente",
-      id_state: "Activo",
-      id_hospital: "hospital 1",
-      creation_date: Date.now(),  
-      updated:"true"
-    }
+      id_local_patient: "id_local_patient",
+    };
     const savedPatient = await createPatient(patient);
-    return savedPatient; 
-  }catch(err){
-    console.log(err)
+    return savedPatient;
+  } catch (err) {
+    console.log(err);
   }
- 
 }
 
 main();
