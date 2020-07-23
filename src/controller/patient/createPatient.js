aae63aa3a99a5e7384f247389ecec2244b2b3f42
GaboDotCodes <gabedu2000@gmail.com>
2020-07-23 17:56:20 -0500

    export default || New file

--- src/controller/patient/createPatient.js
+++ src/controller/patient/createPatient.js
@@ -1,8 +1,9 @@
-import Patient from '../../models/patient.model'
+import Patient from "../../models/patient.model";
 
-export const createPatient = async (patient) => {
+const createPatient = async (patient) => {
   const newPatient = new Patient(patient);
   const patientReturn = await newPatient.save();
   return patientReturn;
 };
 
+export default createPatient;
