71bf8d276dffef308ea59ab05c5132110f9c1e1d
GaboDotCodes <gabedu2000@gmail.com>
2020-07-24 09:44:52 -0500

    getPatient Update

--- src/controller/patient/getPatient.js
+++ src/controller/patient/getPatient.js
@@ -1,11 +1,11 @@
-import Patient from '../../models/patient.model'
+import Patient from '../../models/patient.model';
 
 export const getPatientByIdLocal = async (id) => {
-  const patientReturn = await Patient.findOne({ "id_local_patient":id });
+  const patientReturn = await Patient.findOne({ id_local_patient: id });
   return patientReturn;
 };
 
 export const getPatientIdGlobal = async (id) => {
-  const patientReturn = await Patient.findOne({ "id_global_patient":id });
+  const patientReturn = await Patient.findOne({ id_global_patient: id });
   return patientReturn;
 };
