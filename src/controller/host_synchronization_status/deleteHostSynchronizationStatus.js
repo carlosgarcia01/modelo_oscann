04cc697f77158e1e25d1611ad4a4c726366535b8
GaboDotCodes <gabedu2000@gmail.com>
2020-07-24 12:46:11 -0500

    Change export

--- src/controller/host_synchronization_status/deleteHostSynchronizationStatus.js
+++ src/controller/host_synchronization_status/deleteHostSynchronizationStatus.js
@@ -1,8 +1,8 @@
 import SyncHostState from '../../models/host_synchronization_status.model';
 
-const deleteSyncHostState = async (idSyncHostState) => {
+const deleteHostSynchronizationStatus = async (idSyncHostState) => {
   const deletedSyncHostState = await SyncHostState.findByIdAndDelete(idSyncHostState);
   return deletedSyncHostState;
 };
 
-export default deleteSyncHostState;
+export default deleteHostSynchronizationStatus;
