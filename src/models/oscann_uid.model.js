dcbe9229cab76d194bdc07e60e1eee11f273b4b9
GaboDotCodes <gabedu2000@gmail.com>
2020-07-24 09:26:17 -0500

    OscannUid Update

--- src/models/oscann_uid.model.js
+++ src/models/oscann_uid.model.js
@@ -1,36 +1,34 @@
-import { Schema, model } from "mongoose";
-import User from './user.model';
-import State from './state.model';
+import { Schema, model } from 'mongoose';
 
 const OscannUidSchema = new Schema({
   name: String,
   instalation_date: Date,
   version: String,
   update_date: Date,
-  uid_oscann:{
+  uid_oscann: {
     type: String,
     required: true,
   },
   id_user: {
-    type: Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: false, 
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: false,
   },
-  id_state: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'State', 
-    required: false, 
+  id_state: {
+    type: Schema.Types.ObjectId,
+    ref: 'State',
+    required: false,
   },
   creation_date: {
     type: Date,
     default: Date.now(),
     required: true,
   },
-  updated:{
-    type:Boolean,
-    default:false,
-    required:false
-  }
-})
+  updated: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+});
 
 export default model('OscannUid', OscannUidSchema);
