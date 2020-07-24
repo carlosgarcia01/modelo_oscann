fe6df3055622baeb599afb2cc366e01eba294aa8
GaboDotCodes <gabedu2000@gmail.com>
2020-07-24 09:27:03 -0500

    Process Update

--- src/models/process.model.js
+++ src/models/process.model.js
@@ -1,23 +1,22 @@
-import { Schema, model } from "mongoose";
-import State from './state.model';
+import { Schema, model } from 'mongoose';
 
 const ProcessSchema = new Schema({
   description: String,
   update_date: Date,
-  id_state: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'State', 
-    required: false, 
+  id_state: {
+    type: Schema.Types.ObjectId,
+    ref: 'State',
+    required: false,
   },
   creation_date: {
     type: Date,
     default: Date.now(),
   },
-  updated:{
-    type:Boolean,
-    default:false,
-    required:false
-  }
-})
+  updated: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+});
 
 export default model('Process', ProcessSchema);
