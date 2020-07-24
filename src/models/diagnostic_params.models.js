e1c3550737bb26686ac6e72342b99f099fd7436a
GaboDotCodes <gabedu2000@gmail.com>
2020-07-24 09:25:00 -0500

    DiagnosticParams Update

--- src/models/diagnostic_params.models.js
+++ src/models/diagnostic_params.models.js
@@ -1,7 +1,4 @@
-import { Schema, model } from "mongoose";
-import TypeResult from './result_type.model';
-import StudyCatalog from './study_catalog.model';
-import State from './state.model';
+import { Schema, model } from 'mongoose';
 
 const DiagnosticParamsSchema = new Schema({
   name: String,
@@ -9,29 +6,29 @@ const DiagnosticParamsSchema = new Schema({
   conclusion: String,
   update_date: Date,
   id_type_result: {
-    type: Schema.Types.ObjectId, 
-    ref: 'TypeResult', 
+    type: Schema.Types.ObjectId,
+    ref: 'TypeResult',
     required: false,
   },
   id_study_catalog: {
-    type: Schema.Types.ObjectId, 
-    ref: 'StudyCatalog', 
+    type: Schema.Types.ObjectId,
+    ref: 'StudyCatalog',
     required: false,
   },
-  id_state: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'State', 
-    required: false, 
+  id_state: {
+    type: Schema.Types.ObjectId,
+    ref: 'State',
+    required: false,
   },
   creation_date: {
     type: Date,
     default: Date.now(),
   },
-  updated:{
-    type:Boolean,
-    default:false,
-    required:false
-  }
-})
+  updated: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+});
 
 export default model('DiagnosticParams', DiagnosticParamsSchema);
