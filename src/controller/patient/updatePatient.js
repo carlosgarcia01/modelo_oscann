d0449b9ea0cfce924a0a2197e862c2482cf514bc
GaboDotCodes <gabedu2000@gmail.com>
2020-07-24 09:51:54 -0500

    updatePatient Update

--- src/controller/patient/updatePatient.js
+++ src/controller/patient/updatePatient.js
@@ -1,9 +1,13 @@
-import Patient from '../../models/patient.model'
+import Patient from '../../models/patient.model';
 
-export  const updatePatient = async (id, patient,) => {
-  return await Patient.findByIdAndUpdate(id, patient, { new: true });
+export const updatePatient = async (id, patient) => {
+  const patientUpdated = await Patient.findByIdAndUpdate(id, patient, { new: true });
+  return patientUpdated;
 };
 
-export  const updatePatientIdGlobal = async (patient) => {
-  return await Patient.findOneAndUpdate({ "id_global_patient":id }, patient, { new: true });
+export const updatePatientIdGlobal = async (id, patient) => {
+  const patientUpdated = await Patient.findOneAndUpdate({ id_global_patient: id }, patient, {
+    new: true,
+  });
+  return patientUpdated;
 };
