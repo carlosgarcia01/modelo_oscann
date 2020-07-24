ac092bfc4c3b0d0172b66cfc43efdb387200ca33
GaboDotCodes <gabedu2000@gmail.com>
2020-07-24 09:28:07 -0500

    Serie Update

--- src/models/serie.model.js
+++ src/models/serie.model.js
@@ -1,17 +1,12 @@
-import { Schema, model } from "mongoose";
-import User from './user.model';
-import Serie_catalog from './serie_catalog.model';
-import Oscann from './oscann_uid.model';
-import Study from './study.model';
-import State from './state.model';
+import { Schema, model } from 'mongoose';
 
 const SerieSchema = new Schema({
-  update_date:Date ,
-  completion_date:Date,
-  finish_time:Date,
+  update_date: Date,
+  completion_date: Date,
+  finish_time: Date,
   duration: Number,
   video: String,
-  observation:String,
+  observation: String,
   valid: String,
   blinks: String,
   gazy_rawx: String,
@@ -24,57 +19,57 @@ const SerieSchema = new Schema({
   stimuluy: String,
   stimulux: String,
   gazy: String,
-  gazx:String ,
+  gazx: String,
   time: String,
   id_global_serie: {
     type: String,
-    required: true
+    required: true,
   },
   id_user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
-    required: false 
-  } ,
+    required: false,
+  },
   id_serie_catalog: {
     type: Schema.Types.ObjectId,
     ref: 'Serie_catalog',
-    required: false 
+    required: false,
   },
   id_oscann: {
     type: Schema.Types.ObjectId,
-    ref: 'Oscann', 
-    required: false 
+    ref: 'Oscann',
+    required: false,
   },
   id_study: {
-    type:Schema.Types.ObjectId,
-    ref:'Study',
-    required:false 
+    type: Schema.Types.ObjectId,
+    ref: 'Study',
+    required: false,
   },
-  id_state:{
-    type:Schema.Types.ObjectId,
-    ref:'State',
-    required:false 
+  id_state: {
+    type: Schema.Types.ObjectId,
+    ref: 'State',
+    required: false,
   },
-  serie:{
-    type:Boolean,
-    default:true,
-    required:false,
+  serie: {
+    type: Boolean,
+    default: true,
+    required: false,
   },
   creation_date: {
     type: Date,
     default: Date.now(),
-    required:false,
+    required: false,
   },
   start_time_serie: {
-    type:Date,
+    type: Date,
     default: Date.now(),
-    required:false,
+    required: false,
   },
-  updated:{
-    type:Boolean,
-    default:false,
-    required:false
+  updated: {
+    type: Boolean,
+    default: false,
+    required: false,
   },
-})
+});
 
 export default model('Serie', SerieSchema);
