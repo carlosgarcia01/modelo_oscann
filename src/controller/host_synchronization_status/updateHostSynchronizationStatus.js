cad3f905da2b53136337f15f9b3f7a144af2c4d2
GaboDotCodes <gabedu2000@gmail.com>
2020-07-24 12:46:28 -0500

    Change export

--- src/controller/host_synchronization_status/updateHostSynchronizationStatus.js
+++ src/controller/host_synchronization_status/updateHostSynchronizationStatus.js
@@ -1,8 +1,8 @@
 import SyncHostState from '../../models/host_synchronization_status.model';
 
-const updateSyncHostState = async (id, log) => {
+const updateHostSynchronizationStatus = async (id, log) => {
   const logUpdated = await SyncHostState.findByIdAndUpdate(id, log, { new: true });
   return logUpdated;
 };
 
-export default updateSyncHostState;
+export default updateHostSynchronizationStatus;
